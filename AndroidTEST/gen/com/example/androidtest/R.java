b97af4359c22bca2381848165cf891585a30dd65
oevad <dccoao13@yahoo.com>
2013-11-16 09:42:15 -0500

    button 2 and 3 are wired.
    Button 2 shows toast notification.
    Button 3 "closes" the application.
    Cleaned up almost all of the warnings.

--- AndroidTEST/gen/com/example/androidtest/R.java
+++ AndroidTEST/gen/com/example/androidtest/R.java
@@ -39,10 +39,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int btn1text=0x7f050005;
-        public static final int btn2text=0x7f050004;
-        public static final int btn3text=0x7f050003;
-        public static final int hello_world=0x7f050002;
+        public static final int btn1Text=0x7f050002;
+        public static final int btn2Text=0x7f050003;
+        public static final int btn3Text=0x7f050004;
+        public static final int messageString1=0x7f050005;
+        public static final int messageString2=0x7f050006;
+        public static final int messageString3=0x7f050007;
     }
     public static final class style {
         /** 
